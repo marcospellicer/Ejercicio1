a9c65f7bf4039a980f90546acf98a244b49bbea4
alumno <alumno@aula01-09>
2018-03-06 20:45:09 +0100

    operaciones

---
+++ Ejercicio1/src/ejercicio1/Operaciones.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+
+/**
+ *
+ * @author alumno
+ */
+public class Operaciones {
+   
+    public static String o1(){
+     String aux=""; 
+     
+        for (int i = 1; i < 21; i++) {
+            aux+=i+"\n";
+        }
+        System.out.println(aux);
+        return aux;
+    }
+    
+}
